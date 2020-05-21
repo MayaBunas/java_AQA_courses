a02725e25f3d9792be4279457081cae567d80322
MayaBunas <maya.bunas@users.noreply.github.com>
2020-05-21 23:15:10 +0300

    Reformate code

--- sandbox/src/main/java/by/stqa/pft/homework2/Point.java
+++ sandbox/src/main/java/by/stqa/pft/homework2/Point.java
@@ -5,13 +5,13 @@ public class Point {
   double x;
   double y;
 
-  public Point (double x, double y){
+  public Point(double x, double y) {
     this.x = x;
     this.y = y;
   }
 
-  public double distance (Point p){
-    return Math.sqrt((this.x-p.x)*(this.x-p.x) + (this.y-p.y)*(this.y-p.y));
+  public double distance(Point p) {
+    return Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y));
   }
 
   @Override

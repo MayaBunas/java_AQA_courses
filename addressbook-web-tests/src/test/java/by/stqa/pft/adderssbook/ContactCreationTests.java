993fad578f44e0f0edb3b5770c6cbabcfc35bdd0
MayaBunas <maya.bunas@users.noreply.github.com>
2020-05-29 00:01:38 +0300

    Change group name for the contact
    Homework 5. Preparation for deletion.

--- addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/ContactCreationTests.java
@@ -40,7 +40,7 @@ public class ContactCreationTests {
             "Best British Female Artist", "Club 27", "Southgate, London",
             "+44 20 7123 1234", "+44 20 7777 7777", "+44 20 7111 1111", "+44 20 7666 6666",
             "amy.winehouse@club27.com", "amy@rehab.com", "amy@winehouse.com", "https://en.wikipedia.org/wiki/Amy_Winehouse",
-            "14", "September", "1983", "23", "July", "2011", "Club27",
+            "14", "September", "1983", "23", "July", "2011", "The Club 27",
             "Camden, London", "Heaven", "Amy was the best!"));
     submitContactCreation();
     returnToHomePage();

1a0e5874a2c95a2ce2e06b0a671d783928461d70
MayaBunas <maya.bunas@users.noreply.github.com>
2020-05-29 00:42:04 +0300

    Add logout() helping method
    Homework 4.

--- addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/ContactCreationTests.java
@@ -137,8 +137,12 @@ public class ContactCreationTests {
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    wd.findElement(By.linkText("Logout")).click();
+    logout();
     wd.quit();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
   }
 
   private boolean isElementPresent(By by) {

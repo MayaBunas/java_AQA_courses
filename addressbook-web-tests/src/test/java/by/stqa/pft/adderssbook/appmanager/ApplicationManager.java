3751296e3610fd3f6bec6ca7963ac4c63d73cecb
MayaBunas <maya.bunas@users.noreply.github.com>
2020-06-01 07:14:11 +0300

    Implement the ability to run tests in different browsers
    Lessons 3.3 and 3.4. Homework 7.

--- addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/appmanager/ApplicationManager.java
@@ -2,6 +2,9 @@ package by.stqa.pft.adderssbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,13 +12,25 @@ public class ApplicationManager {
 
   WebDriver wd;
 
+  private String browser;
+
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    wd = new ChromeDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     sessionHelper = new SessionHelper(wd);

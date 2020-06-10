54aa25d43709134822ed0c9ac46b9f707eb067fa
MayaBunas <maya.bunas@users.noreply.github.com>
2020-06-10 23:42:46 +0300

    Add lists equality check for Deletion tests
    Lesson 4.6. Override toString, equals andhashCode methods.

--- addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package by.stqa.pft.adderssbook.model;
 
+import java.util.Objects;
+
 public class ContactData {
   private final String firstName;
   private final String middleName;
@@ -157,5 +159,29 @@ public class ContactData {
 
   public String getNotes() {
     return notes;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", address='" + address + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return Objects.equals(firstName, that.firstName) &&
+            Objects.equals(lastName, that.lastName) &&
+            Objects.equals(address, that.address);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName, address);
   }
 }

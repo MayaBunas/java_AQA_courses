570761b4be7260f556b8699e3f8d108ff9706d29
MayaBunas <maya.bunas@users.noreply.github.com>
2020-06-25 16:20:21 +0300

    Add data provider to Group and Contact creation tests
    Homework 13. Lessons 6.4, 6.5, partly lessons 6.6 and 6.7.

--- addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/tests/ContactCreationTests.java
@@ -3,16 +3,56 @@ package by.stqa.pft.adderssbook.tests;
 import by.stqa.pft.adderssbook.model.ContactData;
 import by.stqa.pft.adderssbook.model.Contacts;
 import by.stqa.pft.adderssbook.model.GroupData;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType());
+      return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+  }
 
   @BeforeMethod
   public void ensurePreconditions() {
@@ -25,20 +65,11 @@ public class ContactCreationTests extends TestBase {
     app.goTo().homePage();
   }
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.contact().getSet();
     File photo = new File("src/test/resources/amy_winehouse.jpg");
-    ContactData contact = new ContactData().withFirstName("Amy").withMiddleName("Jade").withLastName("Winehouse")
-            .withNickname("Rehab").withPhoto(photo).withTitle("Best British Female Artist").withCompany("Club 27")
-            .withAddress("Southgate, London").withHomePhone("+44 20 7123 1234").withMobilePhone("+44 20 7777 7777")
-            .withWorkPhone("+44 20 7111 1111").withFax("+44 20 7666 6666").withEmail1("amy.winehouse@club27.com")
-            .withEmail2("amy@rehab.com").withEmail3("amy@winehouse.com")
-            .withHomepage("https://en.wikipedia.org/wiki/Amy_Winehouse")
-            .withBday("14").withBmonth("September").withByear("1983").withAday("23").withAmonth("July").withAyear("2011")
-            .withGroup("The Club 27").withAddress2("Camden, London").withsecondHomePhone("+ (333) 333 - 333 - 333")
-            .withNotes("Amy was the best!");
-    app.contact().create(contact);
+    app.contact().create(contact.withPhoto(photo));
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().getSet();
     assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
--- addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/adderssbook/tests/GroupCreationTests.java
@@ -2,20 +2,60 @@ package by.stqa.pft.adderssbook.tests;
 
 import by.stqa.pft.adderssbook.model.GroupData;
 import by.stqa.pft.adderssbook.model.Groups;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(GroupData.class);
+      List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+  }
+
+  @Test(dataProvider = "validGroupsFromJson")
+  public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().getSet();
-    GroupData group = new GroupData().withName("The Club 27")
-            .withHeader("The 27 Club is a list consisting mostly of popular musicians, artists, or actors who died at age 27.")
-            .withFooter("Brian Jones, Jimi Hendrix, Janis Joplin, Jim Morrison, Kurt Cobain, Amy Winehouse.");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().getSet();

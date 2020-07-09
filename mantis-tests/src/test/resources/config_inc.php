20649ca6c9ffefd301270f0afa167beaa4dd598c
MayaBunas <maya.bunas@users.noreply.github.com>
2020-07-09 21:46:46 +0300

    Create Registration Test
    Lesson 8.6. Read emails and can find there a link

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -6,3 +6,5 @@ $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
 $g_crypto_master_salt = 'ivFulvyV6lWqfif3';
+$v_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';

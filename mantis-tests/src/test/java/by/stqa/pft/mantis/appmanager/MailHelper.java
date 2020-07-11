54c0b70fc94b169023b20efbdcadbf3db834d45c
MayaBunas <maya.bunas@users.noreply.github.com>
2020-07-11 18:26:14 +0300

    Make registration through external server
    Lesson 8.7. Telnet should be turned on on your computer!
    Direct link to James 2.3.1 (just anarchive and use):
    http://archive.apache.org/dist/james/server/james-binary-2.3.1.zip

--- mantis-tests/src/test/java/by/stqa/pft/mantis/appmanager/MailHelper.java
+++ mantis-tests/src/test/java/by/stqa/pft/mantis/appmanager/MailHelper.java
@@ -3,6 +3,7 @@ package by.stqa.pft.mantis.appmanager;
 import by.stqa.pft.mantis.model.MailMessage;
 import org.subethamail.wiser.Wiser;
 import org.subethamail.wiser.WiserMessage;
+import ru.lanwen.verbalregex.VerbalExpression;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -46,6 +47,13 @@ public class MailHelper {
       return null;
     }
   }
+
+  public String findLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    return regex.getText(mailMessage.text);
+  }
+
 
   public void start() {
     wiser.start();

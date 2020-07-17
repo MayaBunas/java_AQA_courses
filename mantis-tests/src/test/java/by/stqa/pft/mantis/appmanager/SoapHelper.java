3b6e8784e8f4848d13e4cbae7ebde27e4e068992
MayaBunas <maya.bunas@users.noreply.github.com>
2020-07-17 13:10:05 +0300

    Add Issue Status dependency in Mantis Tests
    Homework 19.

--- mantis-tests/src/test/java/by/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/by/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -49,4 +49,10 @@ public class SoapHelper {
             .withProject(new Project().withId(createdIssueData.getProject().getId().intValue())
                     .withName(createdIssueData.getProject().getName()));
   }
+
+  public String issueStatus(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    MantisConnectPortType mc = getMantisConnect();
+    IssueData issueData = mc.mc_issue_get("administrator", "root", BigInteger.valueOf(issueId));
+    return issueData.getStatus().getName();
+  }
 }
--- mantis-tests/src/test/java/by/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/by/stqa/pft/mantis/tests/TestBase.java
@@ -2,11 +2,17 @@ package by.stqa.pft.mantis.tests;
 
 import by.stqa.pft.mantis.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestBase {
 
@@ -22,5 +28,17 @@ public class TestBase {
   public void tearDown() throws IOException {
     app.ftp().restore("config_inc.php.bak", "config_inc.php");
     app.stop();
+  }
+
+  public boolean isIssueOpen(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    String status = app.soap().issueStatus(issueId);
+    List<String> readyForQa = Arrays.asList("resolved", "closed");
+    return !readyForQa.contains(status);
+  }
+
+  public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
   }
 }
